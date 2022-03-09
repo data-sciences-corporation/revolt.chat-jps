6c5f230bb28408b90cb5f5ce1c8a17b7f4a13e75
Richard Raymond <theplacetosendmemail@gmail.com>
2022-03-09 13:59:22 +0200

    Added revolt launcher

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 fqdn=$1
 public_ip=$2
 echo "Creating directories" >> INSTALL_LOG
-mkdir -p /root/revolt/data
+git clone https://github.com/revoltchat/self-hosted revolt
 chown -R docker. /root/revolt
+#docker-compose up -d
 echo "Deploying container on IP [$public_ip] and URL [$fqdn]" >> INSTALL_LOG
